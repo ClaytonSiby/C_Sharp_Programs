e8c561300137ae65a7a0bbc719192c2516d024ee
ClaytonSiby <claytonsiby@gmail.com>
2020-07-26 23:14:54 +0200

    add languages file

---
+++ LanguageBasics/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LanguageBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Clayton";
+            Console.WriteLine($"How old are you, {name}? >> ");
+            string age = Console.ReadLine();
+            Console.WriteLine($"So {name} you're {Convert.ToInt32(age)} years old.");
+        }
+    }
+}
